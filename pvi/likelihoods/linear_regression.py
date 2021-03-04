269a6ad264367d1afee17c88049f5f8d980d05e8
MattAshman <matthew.ashman97@gmail.com>
2021-03-04 10:34:29 +0000

    Adding Bayesian linear regression model

--- pvi/likelihoods/linear_regression.py
+++ pvi/likelihoods/linear_regression.py
@@ -3,8 +3,8 @@ from torch.nn import functional as F
 from .base import Likelihood
 
 
-class LogisticRegressionLikelihood(Likelihood):
-    def __init__(self, output_sigma):
+class LinearRegressionLikelihood(Likelihood):
+    def __init__(self, output_sigma=1.):
         super().__init__()
 
         # Keep fixed, for now.
