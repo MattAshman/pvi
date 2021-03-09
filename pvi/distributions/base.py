2347803a4b4f84d911944f019a14163b07bdfa80
stratisMarkou <stratismar@gmail.com>
2021-03-09 13:47:40 +0200

    fixed approximating likelihood terms with new API

--- pvi/distributions/base.py
+++ pvi/distributions/base.py
@@ -1,42 +1,121 @@
 from abc import ABC, abstractmethod
 from torch import nn
 
-
 # =============================================================================
-# Base approximating likelihood class
+# Base class for approximating likelihood factors of the exponential family
 # =============================================================================
 
 
-class ApproximatingLikelihoodFactor(ABC):
+class ExponentialFamilyFactor(ABC):
+    """
+    Base class for exponential family (EF) approximating likelihood factors.
+    The exponential family is made up of distributions which can be written
+    in the form
     
-    # TODO:
-    # - deal with the initialisation t(θ) = 1
+        p(x | θ) = h(x) exp(ν.T f(x) + A(ν)).
     
-    def __init__(self, log_coefficient, natural_parameters):
+    For a list of exponential family distributions see:
+        https://en.wikipedia.org/wiki/Category:Exponential_family_distributions
+        https://en.wikipedia.org/wiki/Exponential_family#Table_of_distributions
+    
+    
+    TODO: Write children classes for the following members of the EF
+    
+        - Dirichlet
+        - Multinomial
         
-        # Set leading coefficient and natural parameters
-        self.log_coefficient = log_coefficient
+    There are many more members but these are especially useful for our
+    applications.
+    """
+    
+    ### TODO: write __mul__ and __rmul__ for extra cleanliness
+    
+    def __init__(self, natural_parameters):
+        
         self.natural_parameters = natural_parameters
+      
+    
+    def compute_refined_factor(self, q1, q2):
+        """
+        Computes the log-coefficient and natural parameters of the
+        approximating likelihood term **t** given by
+        
+            t(θ) = q1(θ) / q2(θ) t_(θ)
+            
+        where **t_** is the approximating likelihood term corresponding
+        to **self**. Note that the log-coefficient computed here includes
+        the normalising constants of the q-distributions as well as the
+        coefficient of t_.
+        """
+        
+        # Convert distributions to log-coefficients and natural parameters
+        np1 = self.np_from_distribution(q1)
+        np2 = self.np_from_distribution(q2)
+        
+        # Log coefficient and natural parameters of refined factor
+        natural_parameters = {}
+        
+        # Compute natural parameters of the new t-factor
+        for name, np in self.natural_parameters.items():
+            natural_parameters[name] = np1[name] - np2[name] + np
+            
+        # Create and return refined t of the same type
+        t = type(self)(natural_parameters)
+        
+        return t
+    
+
+    def __call__(self, thetas):
+        """
+        Returns the value of log t(θ) where
+        
+            log t(θ) = log c + log h(x) + ν.T f(x)
+            
+        Input **thetas** is assumed to be a torch.tensor of shape (N, D)
+        where N is the batch dimension and D is the dimension of the
+        distribution.
+        """
+        
+        # Compute inner product ν.T f(x), log h(θ), log t(θ) (all shape (N,))
+        npf = self.npf(thetas)
+        log_h = self.log_h(thetas)
+        log_t = log_h + npf
+        
+        return log_t
     
     
     @abstractmethod
-    def __call__(self, thetas):
+    def log_h(self, thetas):
         """
-        Computes the log-value of θ under the factor t, i.e. log t(θ).
+        Returns the value of log h(θ). Input **thetas** is assumed to be
+        a torch.tensor of shape (N, D) where N is the batch dimension and
+        D is the dimension of the distribution.
         """
         pass
     
     
     @abstractmethod
-    def compute_refined_factor(self, q, q_):
+    def npf(self, thetas):
         """
-        Computes the natural parameters and leading coefficient of the
-        approximating likelihood term **t** given by
-        
-            t(θ) = q(θ) / q_(θ) t_(θ)
-            
-        where **t_** is the approximating likelihood term corresponding
-        to **self**.
+        Rearranges NPs to ν vector for this member of the exponential family.
+        """
+        pass
+    
+    
+    @abstractmethod
+    def np_from_distribution(self, q):
+        """
+        Takes a torch.distribution **q**, assumed to be in the EF
+        and extracts its leading coefficient and natural parameters.
+        """
+        pass
+    
+    
+    @abstractmethod
+    def distribution_from_np(self, np):
+        """
+        Takes a dictionary of natural parameters **np** and returns a
+        torch.distribution defined by these natural parameters.
         """
         pass
     
@@ -54,9 +133,29 @@ class ApproximatingLikelihoodFactor(ABC):
 #                  natural_parameters=None
 #                  is_trainable=False):
         
-#         # Initialise using either standard parameters or natural parameters
-#         if not (parameters is None):
-#             self.a
+#         # Initialise q using standard parameters
+#         pass
+    
+    
+#     @property
+#     def standard_parameters(self):
+#         pass
+    
+    
+#     @property
+#     def natural_parameters(self):
+#         pass
+    
+    
+#     @abstractmethod
+#     def std_params_to_nat_params(self):
+#         pass
+    
+    
+#     @abstractmethod
+#     def nat_params_to_std_params(self):
+#         pass
+        
     
 #     @property
 #     @abstractmethod
@@ -65,9 +164,6 @@ class ApproximatingLikelihoodFactor(ABC):
     
     
 #     def log_prob(self, thetas):
-#         """
-#         Computes the log-probability of the distribution log q(θ).
-#         """
 #         return self.q.log_prob(thetas)
     
     
@@ -75,14 +171,10 @@ class ApproximatingLikelihoodFactor(ABC):
 #         return self.q.kl_divergence(q_)
     
     
-#     def get_natural_parameters(self):
-#         return self.natural_parameters
-    
-    
 #     def sample(self):
 #         return self.q.sample()
     
     
-#     def
-        
+#     def rsample(self):
+#         return self.q.rsample()
             
--- pvi/distributions/exponential_family_factors.py
+++ pvi/distributions/exponential_family_factors.py
@@ -1,131 +1,9 @@
 from abc import ABC, abstractmethod
-from .base import ApproximatingLikelihoodFactor
+from .base import ExponentialFamilyFactor
 
 import math
 
 import torch
-
-
-# =============================================================================
-# Base class for approximating likelihood factors of the exponential family
-# =============================================================================
-
-
-class ExponentialFamilyFactor(ApproximatingLikelihoodFactor):
-    """
-    Base class for exponential family (EF) approximating likelihood factors.
-    The exponential family is made up of distributions which can be written
-    in the form
-    
-        p(x | θ) = h(x) exp(ν.T f(x) + A(ν)).
-    
-    For a list of exponential family distributions see:
-        https://en.wikipedia.org/wiki/Category:Exponential_family_distributions
-        https://en.wikipedia.org/wiki/Exponential_family#Table_of_distributions
-    
-    
-    TODO: Write children classes for the following members of the EF
-    
-        - MultivariateGaussian
-        - Laplace
-        - Dirichlet
-        - Multinomial
-        
-    There are many more members but these are especially useful for our
-    applications.
-    """
-    
-    def __init__(self, log_coefficient, natural_parameters):
-        
-        super().__init__(log_coefficient=log_coefficient,
-                         natural_parameters=natural_parameters)
-      
-    
-    def compute_refined_factor(self, q1, q2):
-        """
-        Computes the log-coefficient and natural parameters of the
-        approximating likelihood term **t** given by
-        
-            t(θ) = q1(θ) / q2(θ) t_(θ)
-            
-        where **t_** is the approximating likelihood term corresponding
-        to **self**. Note that the log-coefficient computed here includes
-        the normalising constants of the q-distributions as well as the
-        coefficient of t_.
-        """
-        
-        # Convert distributions to log-coefficients and natural parameters
-        log_coeff1, np1 = self.log_coeff_and_np_from_distribution(q1)
-        log_coeff1, np2 = self.log_coeff_and_np_from_distribution(q2)
-        
-        # Log coefficient and natural parameters of refined factor
-        log_coeff = log_coeff1 - log_coeff2 + self.log_coeff
-        natural_parameters = {}
-        
-        # Compute natural parameters of the new t-factor
-        for name, natural_parameter in self.natural_parameters.items():
-            natural_parameters[name] = np1[name] - np2[name] + np
-            
-        # Create and return refined t of the same type
-        t = type(self).__init__(log_coeff, natural_parameters)
-        
-        return t
-    
-
-    def __call__(self, thetas):
-        """
-        Returns the value of log t(θ) where
-        
-            log t(θ) = log c + log h(x) + ν.T f(x)
-            
-        Input **thetas** is assumed to be a torch.tensor of shape (N, D)
-        where N is the batch dimension and D is the dimension of the
-        distribution.
-        """
-        
-        # Compute inner product ν.T f(x), log h(θ), log t(θ) (all shape (N,))
-        npf = self.npf(thetas)
-        log_h = self.log_h(thetas)
-        log_t = self.log_coefficient + log_h + npf
-        
-        return log_t
-    
-    
-    @abstractmethod
-    def log_h(self, thetas):
-        """
-        Returns the value of log h(θ). Input **thetas** is assumed to be
-        a torch.tensor of shape (N, D) where N is the batch dimension and
-        D is the dimension of the distribution.
-        """
-        pass
-    
-    
-    @abstractmethod
-    def npf(self, thetas):
-        """
-        Rearranges NPs to ν vector for this member of the exponential family.
-        """
-        pass
-    
-    
-    @abstractmethod
-    def log_coeff_and_np_from_distribution(self, q):
-        """
-        Takes a torch.distribution **q**, assumed to be in the EF
-        and extracts its leading coefficient and natural parameters.
-        """
-        pass
-    
-    
-    @abstractmethod
-    def distribution_from_np(self, np):
-        """
-        Takes a dictionary of natural parameters **np** and returns a
-        torch.distribution defined by these natural parameters.
-        """
-        pass
-    
 
 
 # =============================================================================
@@ -136,10 +14,8 @@ class ExponentialFamilyFactor(ApproximatingLikelihoodFactor):
 class MeanFieldGaussian(ExponentialFamilyFactor):
     
     
-    def __init__(self, log_coefficient, natural_parameters):
-        
-        super().__init__(log_coefficient=log_coefficient,
-                         natural_parameters=natural_parameters)
+    def __init__(self, natural_parameters):
+        super().__init__(natural_parameters=natural_parameters)
     
     
     def log_h(self, thetas):
@@ -165,7 +41,7 @@ class MeanFieldGaussian(ExponentialFamilyFactor):
         return npf
     
     
-    def log_coeff_and_np_from_distribution(self, q):
+    def np_from_distribution(self, q):
         
         # Exctract loc and scale from torch.distribution
         loc = q.loc.detach()
@@ -174,20 +50,13 @@ class MeanFieldGaussian(ExponentialFamilyFactor):
         assert loc.shape == scale.shape
         assert len(loc.shape) == 1
         
-        # Dimension of distribution
-        D = loc.shape[0]
-        
-        # Compute log coefficient of q
-        log_coeff = torch.log(scale).sum()
-        log_coeff = log_coeff - 0.5 * D * math.log(2 * math.pi)
-        
         # Compute natural parameters for multivariate normal
         np = {
             "np1" : loc / scale ** 2,
             "np2" : - 0.5 * scale ** -2
         }
         
-        return log_coeff, np
+        return np
     
     
     def distribution_from_np(self, np):
@@ -212,9 +81,9 @@ class MeanFieldGaussian(ExponentialFamilyFactor):
 class MultivariateGaussian(ExponentialFamilyFactor):
     
     
-    def __init__(self, log_coefficient, natural_parameters):
+    def __init__(self, natural_parameters):
         
-        super().__init__(log_coefficient, natural_parameters)
+        super().__init__(natural_parameters)
     
     
     def log_h(self, thetas):
@@ -240,7 +109,7 @@ class MultivariateGaussian(ExponentialFamilyFactor):
         return npf
     
     
-    def log_coeff_and_np_from_distribution(self, q):
+    def np_from_distribution(self, q):
         """
         Takes a torch.distribution **q**, assumed to be in the EF
         and extracts its leading coefficient and natural parameters.
@@ -250,19 +119,12 @@ class MultivariateGaussian(ExponentialFamilyFactor):
         loc = q.loc.detach()
         scale_tril = q.scale_tril.detach()
         
-        assert loc.shape == scale.shape[0]
+        assert loc.shape[0] == scale_tril.shape[0]
         assert len(loc.shape) == 1
         assert len(scale_tril.shape) == 2
         
-        # Dimension of distribution
-        D = loc.shape[0]
-        
-        # Compute log coefficient of q
-        log_coeff = torch.log(torch.diag(scale_tril)).sum()
-        log_coeff = log_coeff - 0.5 * D * math.log(2 * math.pi)
-        
-        np1 = torch.cholesky_solve(mean[:, None], scale_tril).solution[:, 0]
-        np2 = torch.cholesky_inverse(scale_tril, upper=False)
+        np1 = torch.cholesky_solve(loc[:, None], scale_tril)[:, 0]
+        np2 = -0.5 * torch.cholesky_inverse(scale_tril, upper=False)
         
         # Compute natural parameters for multivariate normal
         np = {
@@ -270,7 +132,7 @@ class MultivariateGaussian(ExponentialFamilyFactor):
             "np2" : np2
         }
         
-        return log_coeff, np
+        return np
     
 
     def distribution_from_np(self, np):
