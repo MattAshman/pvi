ce38826625a065fe568b49d7905365e08f8565ae
MattAshman <matthew.ashman97@gmail.com>
2021-03-25 08:12:15 +0000

    Adding asynchronous server implementation, following Mrinank and Michael's code

--- pvi/servers/asynchronous_server.py
+++ pvi/servers/asynchronous_server.py
@@ -1,4 +1,5 @@
 import logging
+import numpy as np
 
 from tqdm.auto import tqdm
 from .base import Server
@@ -6,9 +7,20 @@ from .base import Server
 logger = logging.getLogger(__name__)
 
 
-class SynchronousServer(Server):
+class AsynchronousServer(Server):
+    """
+    Similar to Mrinank's and Michael's implementation.
+
+    In every round, this server samples M (total number of clients) clients,
+    inversely proportional to the amount of data on each client, and updates
+    them one after another (i.e. incorporating the previous clients updates).
+    """
     def __init__(self, model, q, clients, hyperparameters=None):
         super().__init__(model, q, clients, hyperparameters)
+
+        client_probs = 1 / np.array([client.data['x'].shape[0]
+                                     for client in clients])
+        self.client_probs = client_probs
 
     def get_default_hyperparameters(self):
         return {
@@ -25,7 +37,21 @@ class SynchronousServer(Server):
         delta_nps = []
         clients_updated = 0
 
-        for i, client in tqdm(enumerate(self.clients), leave=False):
+        for i in tqdm(range(len(self.clients)), leave=False):
+
+            available_clients = [client.can_update() for client in
+                                 self.clients]
+
+            if not np.any(available_clients):
+                logger.info('All clients report to be finished. Stopping.')
+                break
+
+            client_index = int(
+                np.random.choice(len(self.clients), 1, replace=False,
+                                 p=self.client_probs))
+            logger.debug(f"Selected Client {client_index}")
+            client = self.clients[client_index]
+
             if client.can_upate():
                 logger.debug(f"On client {i + 1} of {len(self.clients)}.")
                 t_i_old = client.t
@@ -37,6 +63,11 @@ class SynchronousServer(Server):
 
                 delta_nps.append(delta_np)
                 clients_updated += 1
+
+            else:
+                logger.debug(f"Skipping client {client_index}, client not "
+                             "avalible to update.")
+                continue
 
         logger.debug("Received client updates. Updating global posterior.")
 
