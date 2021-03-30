17ac085c58d71e5bb7f49ad549357a807f9c8714
MattAshman <matthew.ashman97@gmail.com>
2021-03-30 21:22:11 +0100

    Adding Bayesian variants of servers

--- pvi/servers/sequential_server.py
+++ pvi/servers/sequential_server.py
@@ -95,7 +95,7 @@ class BayesianSequentialServer(BayesianServer):
                 t_new, teps_new = client.fit(self.q, self.qeps)
 
                 # Compute change in natural parameters.
-                q_delta_np = {k: (t_new.nat_params[k] - t_old.nat_params[k])
+                q_delta_np = {k: t_new.nat_params[k] - t_old.nat_params[k]
                               for k in self.q.nat_params.keys()}
                 qeps_delta_np = {
                     k1: {k2: (teps_new.nat_params[k1][k2]
