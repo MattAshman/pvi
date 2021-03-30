1c958eccb3dd2cd24ee5444eb228c4a5c5a58c75
MattAshman <matthew.ashman97@gmail.com>
2021-03-30 15:50:41 +0100

    Adding BayesianSequentialServer implemenntation

--- pvi/servers/sequential_server.py
+++ pvi/servers/sequential_server.py
@@ -1,7 +1,7 @@
 import logging
 
 from tqdm.auto import tqdm
-from .base import Server
+from .base import *
 
 logger = logging.getLogger(__name__)
 
@@ -50,6 +50,87 @@ class SequentialServer(Server):
 
                 # Log q after each update.
                 self.log["q"].append(self.q.non_trainable_copy())
+                self.log["communications"].append(self.communications)
+
+        logger.debug(f"Iteration {self.iterations} complete."
+                     f"\nNew natural parameters:\n{self.q.nat_params}\n.")
+
+        self.iterations += 1
+
+        self.log["clients_updated"].append(clients_updated)
+
+    def should_stop(self):
+        return self.iterations > self.hyperparameters["max_iterations"] - 1
+
+
+class BayesianSequentialServer(BayesianServer):
+    def __init__(self, model, q, qeps, clients, hyperparameters=None):
+        super().__init__(model, q, qeps, clients, hyperparameters)
+
+        self.log["q"].append(self.q.non_trainable_copy())
+        self.log["qeps"].append(self.qeps.non_trainable_copy())
+        self.log["communications"].append(self.communications)
+
+    def get_default_hyperparameters(self):
+        return {
+            **super().get_default_hyperparameters(),
+            "max_iterations": 5,
+            "damping_factor": 1.,
+        }
+
+    def tick(self):
+        if self.should_stop():
+            return False
+
+        logger.debug("Getting client updates.")
+
+        damping = self.hyperparameters["damping_factor"]
+        clients_updated = 0
+
+        for i, client in tqdm(enumerate(self.clients), leave=False):
+            if client.can_update():
+                logger.debug(f"On client {i + 1} of {len(self.clients)}.")
+                t_old = client.t
+                teps_old = client.teps
+                t_new, teps_new = client.fit(self.q, self.qeps)
+
+                # Compute change in natural parameters.
+                q_delta_np = {k: (t_new.nat_params[k] - t_old.nat_params[k])
+                              for k in self.q.nat_params.keys()}
+                qeps_delta_np = {
+                    k1: {k2: (teps_new.nat_params[k1][k2]
+                              - teps_old.nat_params[k1][k2])
+                         for k2 in self.qeps.nat_params[k1].keys()}
+                    for k1 in self.qeps.nat_params.keys()}
+
+                logger.debug(
+                    "Received client update. Updating global posterior.")
+                # Update global posterior.
+                q_new_nps = {k: v + q_delta_np[k] * damping
+                             for k, v in self.q.nat_params.items()}
+                qeps_new_nps = {
+                    k1: {k2: v2 + qeps_delta_np[k1][k2] * damping
+                         for k2, v2 in self.qeps.nat_params[k1].items()}
+                    for k1 in self.qeps.nat_params.keys()}
+                qeps_new_distributions = {
+                    k: type(self.qeps.distributions[k])(
+                        nat_params=v, is_trainable=False)
+                    for k, v in qeps_new_nps.items()}
+
+                # TODO: compatability to copy inducing points as well.
+                # self.q = type(self.q)(nat_params=q_new_nps, is_trainable=False)
+                self.q = type(self.q)(
+                    inducing_locations=self.q.inducing_locations,
+                    nat_params=q_new_nps, is_trainable=False,
+                    train_inducing=False)
+                self.qeps = type(self.qeps)(
+                    distributions=qeps_new_distributions)
+                clients_updated += 1
+                self.communications += 1
+
+                # Log q after each update.
+                self.log["q"].append(self.q.non_trainable_copy())
+                self.log["qeps"].append(self.qeps.non_trainable_copy())
                 self.log["communications"].append(self.communications)
 
         logger.debug(f"Iteration {self.iterations} complete."
