996640f1d98a0b6eb33d559776023865efb45765
Mikko Heikkilä <mikko.a.heikkila@helsinki.fi>
2021-05-05 09:41:38 +0300

    create testing script for dp-pvi
    initial scripts for running tests without dp

---
+++ notebooks/examples/dp_logistic_regression.py
@@ -0,0 +1,227 @@
+"""
+Script for testing private PVI with logistic regression based on DP-SGD/suff.stats pert
+"""
+
+import argparse
+import logging
+import os
+import sys
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import tqdm.auto as tqdm
+from sklearn.model_selection import KFold
+
+module_path = os.path.abspath(os.path.join("../.."))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
+from pvi.models.logistic_regression import LogisticRegressionModel
+#from pvi.clients.synchronous_client import SynchronousClient
+from pvi.clients import Client
+from pvi.servers.sequential_server import SequentialServer
+
+from pvi.distributions.exponential_family_distributions import MeanFieldGaussianDistribution
+from pvi.distributions.exponential_family_factors import MeanFieldGaussianFactor
+
+from utils import *
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+#logger.setLevel(logging.INFO)
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+def main(args, rng_seed):
+
+
+    np.random.seed(rng_seed)
+
+    client_data, valid_set, N, prop_positive, full_data_split = standard_client_split(
+            None, args.clients, args.rho, args.kappa
+            )
+    x_train, x_valid, y_train, y_valid = full_data_split
+
+    logger.info(f'Proportion of positive examples in each client: {np.array(prop_positive).round(2)}')
+    logger.info(f'Total number of examples in each client: {N}')
+
+    # might need some/all if optimising hyperparams
+    model_hyperparameters = {
+        #"D"                        : x_train.shape[1]-10, # ei näytä vaikuttavan mitenkään
+        #"optimiser"                : "Adam",
+        #"optimiser_params"         : {"lr": 1e-3},
+        #"epochs"                   : None,
+        #"batch_size"               : None,#100,
+        #"num_elbo_samples"         : 20,
+        #"num_predictive_samples"   : 10, # only used when use_probit_approximation = False
+    }
+    model_config = {
+            "use_probit_approximation" : True,
+            "num_predictive_samples"   : 10, # only used when use_probit_approximation = False
+            }
+
+    model = LogisticRegressionModel(hyperparameters=model_hyperparameters, config=model_config)
+
+    # note: for DP need to change to use actual sampling, no full data passes
+    client_config = {
+        'batch_size' : 200, # will run through entire data on each epoch using this batch size
+        'damping_factor' : args.damping_factor,
+        'valid_factors' : False, # does this work at the moment?
+        'epochs' : args.n_steps, # = n local epochs, i.e., full passes through local data
+        'optimiser' : 'Adam',
+        'optimiser_params' : {'lr' : args.learning_rate},
+        'lr_scheduler' : 'MultiplicativeLR',
+        'lr_scheduler_params' : { 'lr_lambda' : lambda epoch: 1.},
+        'num_elbo_samples' : 1, # possible to break if this is low?
+        'print_epochs' : 1, # ?
+        'train_model' : False, # no need for having trainable model on client
+        'update_log_coeff' : False, # no need for log coeff in t factors
+    }
+    # prior params, use data dim+1 assuming model adds extra bias dim
+    prior_std_params = {
+        "loc"   : torch.zeros(x_train.shape[1]+1),
+        "scale" : torch.ones(x_train.shape[1]+1),
+    }
+    # initial t-factor params, not proper distributions
+    init_nat_params = {
+        "np1" : torch.zeros(x_train.shape[1] + 1),
+        "np2" : torch.zeros(x_train.shape[1] + 1),
+    }
+
+    # Initialise clients, q and server
+    clients = set_up_clients(model, client_data, init_nat_params, client_config)
+
+    q = MeanFieldGaussianDistribution(std_params=prior_std_params,
+                                      is_trainable=False)
+    server_config = {
+            'max_iterations' : args.n_global_updates,
+            'train_model' : False, # need False here?
+            'model_update_freq': 1,
+            #'hyper_optimiser': 'SGD',
+            #'hyper_optimiser_params': {'lr': 1},
+            #'hyper_updates': 1,
+            }
+
+    # try using initial q also as prior here
+    server = SequentialServer(model=model,
+                              p=q,
+                              init_q=q,
+                              clients=clients,
+                              config=server_config)
+
+    train_res = {}
+    train_res['acc'] = np.zeros((args.n_global_updates))
+    train_res['logl'] = np.zeros((args.n_global_updates))
+    validation_res = {}
+    validation_res['acc'] = np.zeros((args.n_global_updates))
+    validation_res['logl'] = np.zeros((args.n_global_updates))
+    client_train_res = {}
+    client_train_res['elbo'] = np.zeros((args.clients, args.n_global_updates, args.n_steps))
+    client_train_res['logl'] = np.zeros((args.clients, args.n_global_updates, args.n_steps))
+    client_train_res['kl'] = np.zeros((args.clients, args.n_global_updates, args.n_steps))
+
+    i_global = 0
+    logger.info('Starting model training')
+    while not server.should_stop():
+
+        # run training loop
+        server.tick()
+
+        # get client training curves
+        for i_client in range(args.clients):
+            client_train_res['elbo'][i_client,i_global,:] = server.get_compiled_log()[f'client_{i_client}']['training_curves'][server.iterations-1]['elbo']
+            client_train_res['logl'][i_client,i_global,:] = server.get_compiled_log()[f'client_{i_client}']['training_curves'][server.iterations-1]['ll']
+            client_train_res['kl'][i_client,i_global,:] = server.get_compiled_log()[f'client_{i_client}']['training_curves'][server.iterations-1]['kl']
+        
+        # get global train and validation acc & logl
+        train_acc, train_logl = acc_and_ll(server, torch.tensor(x_train).float(), torch.tensor(y_train).float())
+        valid_acc, valid_logl = acc_and_ll(server, valid_set['x'], valid_set['y'])
+        train_res['acc'][i_global] = train_acc
+        train_res['logl'][ i_global] = train_logl
+        validation_res['acc'][i_global] = valid_acc
+        validation_res['logl'][i_global] = valid_logl
+
+        
+        print(f'Train: accuracy {train_acc:.3f}, mean-loglik {train_logl:.3f}\n'
+              f'Valid: accuracy {valid_acc:.3f}, mean-loglik {valid_logl:.3f}\n')
+
+        i_global += 1
+
+
+    # plot all training curves
+    # NOTE: seems like elbo = logl, why? check later with longer runs
+    #plot_training_curves(client_train_res, args.clients)
+
+    #plot_global_curves(validation_res, 'validation set')
+    #print(f'final validation res:\n{validation_res}')
+    return validation_res, train_res, client_train_res, prop_positive
+
+
+
+def plot_global_curves(res, measure):
+    
+    fig,axs = plt.subplots(2,1, figsize=(8,10))
+    axs[0].plot(res['acc'])
+    axs[0].set_title(f'Global model results, {measure}')
+    axs[0].set_ylabel('Acc')
+    axs[0].set_xlabel('Global updates')
+    axs[0].grid()
+    axs[1].plot(res['logl'])
+    axs[1].set_ylabel('Logl')
+    axs[1].set_xlabel('Global updates')
+    axs[1].grid()
+    plt.show()
+
+
+
+def plot_training_curves(client_train_res, clients):
+    """Plotter for training curves
+    """
+    colors = plt.rcParams['axes.prop_cycle']
+    
+    fig, axs = plt.subplots(3,1, figsize=(6,10))
+    measures = ['elbo', 'logl', 'kl']
+    labels = ['ELBO','logl','KL']
+    for i in range(3):
+        axs[i].set_prop_cycle(colors)
+        for i_client in range(clients):
+            axs[i].plot( client_train_res[measures[i]][i_client,:,:].reshape(-1), label=f'client {i_client}')
+        axs[i].legend()
+        axs[i].set_ylabel(labels[i])
+        axs[i].set_xlabel('Training iteration')
+        axs[i].grid()
+    axs[0].set_title(f'Full training curves with {args.n_global_updates} global updates, {args.n_steps} local steps')
+    plt.show()
+
+
+
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="parse args")
+    parser.add_argument('--n_global_updates', default=2, type=int, help='number of global updates')
+    parser.add_argument('-lr', '--learning_rate', default=1e-2, type=float, help='learning rate')
+    parser.add_argument('-batch_size', default=None, type=list, help='list of batch size(s); used only for test loss/acc. None to use actual test set sizes')
+    parser.add_argument('-q', default=[.005], type=list, help='list of sampling fractions for training data')
+    parser.add_argument('--dp_sigma', default=0.0, type=float, help='DP noise magnitude')
+    parser.add_argument('--dp_C', default=200.0, type=float, help='gradient norm bound')
+
+    parser.add_argument('--folder', default='data/adult/', type=str, help='path to combined train-test adult data folder')
+
+    parser.add_argument('--clients', default=4, type=int, help='number of clients')
+    parser.add_argument('--n_steps', default=2, type=int, help='number of local training steps on each iteration')
+    parser.add_argument('-rho', default=.0, type=float, help='data balance factor, in (0,1); 0=equal sizes, 1=small clients have no data')
+    parser.add_argument('-kappa', default=.0, type=float, help='minority class balance factor, 0=no effect')
+    parser.add_argument('--damping_factor', default=1., type=float, help='damping factor in (0,1], 1=no damping')
+
+    args = parser.parse_args()
+
+    main(args, rng_seed=2303)
+
+
+
---
+++ notebooks/examples/utils.py
@@ -0,0 +1,221 @@
+
+import logging
+import os
+import sys
+
+import numpy as np
+from sklearn.model_selection import KFold
+import torch
+
+module_path = os.path.abspath(os.path.join("../.."))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
+from pvi.models.logistic_regression import LogisticRegressionModel
+#from pvi.clients.synchronous_client import SynchronousClient
+from pvi.clients import Client
+from pvi.servers.sequential_server import SequentialServer
+from pvi.distributions.exponential_family_distributions import MeanFieldGaussianDistribution
+from pvi.distributions.exponential_family_factors import MeanFieldGaussianFactor
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+#logger.setLevel(logging.INFO)
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+
+def set_up_clients(model, client_data, init_nat_params, config):
+
+    clients = []
+    # Create clients
+    for i,_client_data in enumerate(client_data):
+        # Data of ith client
+        data = {k : torch.tensor(v).float() for k, v in _client_data.items()}
+        #logger.debug(f"Client {i} data {data['x'].shape}, {data['y'].shape}")
+
+        # Approximating likelihood term of ith client
+        t = MeanFieldGaussianFactor(nat_params=init_nat_params)
+
+        # Create client and store
+        client = Client(data=data, model=model, t=t, config=config)
+        clients.append(client)
+        
+    return clients
+
+
+
+def standard_client_split(dataset_seed, num_clients, client_size_factor, class_balance_factor, total_splits=2, k_split=0):
+    """
+    Args:
+        dataset_seed : seed for client data splitting, None to avoid fixing separate dataset seed
+        total_split : k for k-fold train-validation split
+        k_split : which data split to use
+    """
+
+    # Get data split
+    full_data_split = get_nth_split(total_splits, k_split)
+    x_train, x_valid, y_train, y_valid = full_data_split
+
+    #logger.debug(f'shapes, x_train: {x_train.shape}, y_train: {y_train.shape}, x_valid: {x_valid.shape}, y_valid: {y_valid.shape}')
+
+    # Prepare training data held by each client
+    client_data, N, prop_positive, _ = generate_clients_data(x=x_train,
+                                                             y=y_train,
+                                                             M=num_clients,
+                                                             client_size_factor=client_size_factor,
+                                                             class_balance_factor=class_balance_factor,
+                                                             dataset_seed=dataset_seed)
+
+    # Validation set, to predict on using global model
+    valid_set = {'x' : torch.tensor(x_valid).float(),
+                 'y' : torch.tensor(y_valid).float()}
+
+    return client_data, valid_set, N, prop_positive, full_data_split
+
+
+
+def acc_and_ll(server, x, y):
+    """Calculate model prediction acc & logl
+    """
+
+    pred_probs = server.model_predict(x)
+    pred_probs = pred_probs.mean.detach().numpy()
+    valid_acc = np.mean((pred_probs > 0.5) == y.numpy())
+    
+    probs = torch.clip(torch.tensor(pred_probs), 0., 1.)
+    valid_loglik = torch.distributions.Bernoulli(probs=probs).log_prob(y)
+    valid_loglik = valid_loglik.mean().numpy()
+    
+    return valid_acc, valid_loglik
+
+
+
+def get_nth_split(n_splits, n, folder='../../data/data/adult/'):
+    """data splitter
+    Args:
+        n_splits : k for k-fold split
+        n : which of the n_splits splits to use, returns only single split
+    """
+    # Load inputs and outputs
+    x = np.load(folder + 'x.npy')
+    y = np.load(folder + 'y.npy')[:, 0]
+    
+    # Kfold splitter from sklearn
+    kfold = KFold(n_splits=n_splits, shuffle=False)
+    
+    # Split data to n_splits splits
+    splits = list(kfold.split(x))
+    x_train = x[splits[n][0]]
+    x_valid = x[splits[n][1]]
+
+    splits = list(kfold.split(y))
+    y_train = y[splits[n][0]]
+    y_valid = y[splits[n][1]]
+
+    return x_train, x_valid, y_train,  y_valid
+
+
+def generate_clients_data(x, y, M, client_size_factor, class_balance_factor, dataset_seed):
+        # this function ought to return a list of (x, y) tuples.
+        # you need to set the seed in the main experiment file to ensure that this function becomes deterministic
+
+        if dataset_seed is not None:
+            random_state = np.random.get_state()
+            np.random.seed(dataset_seed)
+
+        if M == 1:
+            client_data = [{"x": x, "y": y}]
+            N_is = [x.shape[0]]
+            props_positive = [np.mean(y > 0)]
+
+            return client_data, N_is, props_positive, M
+
+        if M % 2 != 0: raise ValueError('Num clients should be even for nice maths')
+
+        N = x.shape[0]
+        small_client_size = int(np.floor((1 - client_size_factor) * N/M))
+        big_client_size = int(np.floor((1 + client_size_factor) * N/M))
+
+        class_balance = np.mean(y == 0)
+
+        small_client_class_balance = class_balance + (1 - class_balance) * class_balance_factor
+        small_client_negative_class_size = int(np.floor(small_client_size * small_client_class_balance))
+        small_client_positive_class_size = int(small_client_size - small_client_negative_class_size)
+
+        if small_client_negative_class_size < 0: raise ValueError('small_client_negative_class_size is negative, invalid settings.')
+        if small_client_positive_class_size < 0: raise ValueError('small_client_positive_class_size is negative, invalid settings.')
+
+
+        if small_client_negative_class_size * M/2 > class_balance * N:
+            raise ValueError(f'Not enough negative class instances to fill the small clients. Client size factor:{client_size_factor}, class balance factor:{class_balance_factor}')
+
+        if small_client_positive_class_size * M/2 > (1-class_balance) * N:
+            raise ValueError(f'Not enough positive class instances to fill the small clients. Client size factor:{client_size_factor}, class balance factor:{class_balance_factor}')
+
+
+        pos_inds = np.where(y > 0)
+        zero_inds = np.where(y == 0)
+        
+        assert (len(pos_inds[0]) + len(zero_inds[0])) == len(y), "Some indeces missed."
+
+        y_pos = y[pos_inds]
+        y_neg = y[zero_inds]
+
+        x_pos = x[pos_inds]
+        x_neg = x[zero_inds]
+
+        client_data = []
+
+        # Populate small classes.
+        for i in range(int(M/2)):
+            client_x_pos = x_pos[:small_client_positive_class_size]
+            x_pos = x_pos[small_client_positive_class_size:]
+            client_y_pos = y_pos[:small_client_positive_class_size]
+            y_pos = y_pos[small_client_positive_class_size:]
+
+            client_x_neg = x_neg[:small_client_negative_class_size]
+            x_neg = x_neg[small_client_negative_class_size:]
+            client_y_neg = y_neg[:small_client_negative_class_size]
+            y_neg = y_neg[small_client_negative_class_size:]
+
+            client_x = np.concatenate([client_x_pos, client_x_neg])
+            client_y = np.concatenate([client_y_pos, client_y_neg])
+
+            shuffle_inds = np.random.permutation(client_x.shape[0])
+
+            client_x = client_x[shuffle_inds, :]
+            client_y = client_y[shuffle_inds]
+
+            client_data.append({'x': client_x, 'y': client_y})
+
+        # Recombine remaining data and shuffle.
+        x = np.concatenate([x_pos, x_neg])
+        y = np.concatenate([y_pos, y_neg])
+        shuffle_inds = np.random.permutation(x.shape[0])
+
+        x = x[shuffle_inds]
+        y = y[shuffle_inds]
+
+        # Distribute among large clients.
+        for i in range(int(M/2)):
+            client_x = x[:big_client_size]
+            client_y = y[:big_client_size]
+
+            x = x[big_client_size:]
+            y = y[big_client_size:]
+
+            client_data.append({'x': client_x, 'y': client_y})
+
+        N_is = [data['x'].shape[0] for data in client_data]
+        props_positive = [np.mean(data['y'] > 0) for data in client_data]
+
+        if dataset_seed is not None:
+            np.random.set_state(random_state)
+
+        return client_data, N_is, props_positive, M
+
+
