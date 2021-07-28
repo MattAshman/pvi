0bd5fce4a89410fb6745a75d7099591b03af8979
MattAshman <matthew.ashman97@gmail.com>
2021-07-28 16:20:55 +0100

    Renaming asynchronous_server.py -> ray_servers.py

--- pvi/servers/__init__.py
+++ pvi/servers/__init__.py
@@ -2,7 +2,7 @@ from .base import *
 from .bcm import *
 from .sequential_server import *
 from .synchronous_server import *
-from .asynchronous_server import *
+from .ray_servers import *
 from .continual_learning_server import *
 from .global_vi import *
 from .streaming_vb_server import *
