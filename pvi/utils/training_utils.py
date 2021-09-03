44f445947ae61f6dcc015b9ff2e6550bfab15767
MattAshman <matthew.ashman97@gmail.com>
2021-09-03 10:10:34 +0100

    Adding timer class

--- pvi/utils/training_utils.py
+++ pvi/utils/training_utils.py
@@ -1,3 +1,4 @@
+import time
 import numpy as np
 
 
@@ -6,8 +7,10 @@ class EarlyStopping:
     Returns False if the score doesn't improve after a given patience.
     https://github.com/Bjarten/early-stopping-pytorch/blob/master/pytorchtools.py
     """
-    def __init__(self, patience=5, score_name="elbo", verbose=False, delta=0,
-                 stash_model=False):
+
+    def __init__(
+        self, patience=5, score_name="elbo", verbose=False, delta=0, stash_model=False
+    ):
         """
         :param patience: How long to wait after last time score improved.
         :param verbose: Whether to print message informing of early stopping.
@@ -47,7 +50,7 @@ class EarlyStopping:
 
             # Check whether to stop.
             if len(vals) > self.patience:
-                prev_vals = np.array(vals[-self.patience:])
+                prev_vals = np.array(vals[-self.patience :])
                 ref_val = np.array(vals[-self.patience - 1]) + self.delta
 
                 if np.all(prev_vals < ref_val):
@@ -56,3 +59,75 @@ class EarlyStopping:
                     return False
             else:
                 return False
+
+
+class Timer:
+    def __init__(self):
+        self.tstart = None
+        self.pcstart = None
+        self.ptstart = None
+
+        self.tpaused = None
+        self.pcpaused = None
+        self.ptpaused = None
+        self.paused = False
+
+    def start(self):
+        """
+        Starts an internal timer by recording the current time.
+        """
+        self.tstart = time.time()
+        self.pcstart = time.perf_counter()
+        self.ptstart = time.process_time()
+
+    def pause(self):
+        """
+        Pauses the timer.
+        """
+        if self.tstart is None:
+            raise ValueError("Timer not started.")
+        if self.paused:
+            raise ValueError("Timer is already paused.")
+
+        self.tpaused = time.time()
+        self.pcpaused = time.perf_counter()
+        self.ptpaused = time.process_time()
+        self.paused = True
+
+    def resume(self):
+        """
+        Resumes the timer by adding the pause time to the start time.
+        """
+        if self.tstart is None:
+            raise ValueError("Timer not started.")
+        if not self.paused:
+            raise ValueError("Timer is not paused.")
+
+        pauset = time.time() - self.tpaused
+        pausepc = time.perf_counter() - self.pcpaused
+        pausept = time.process_time() - self.ptpaused
+
+        self.tstart = self.tstart + pauset
+        self.pcstart = self.pcstart + pausepc
+        self.ptstart = self.ptstart + pausept
+        self.paused = False
+
+    def get(self):
+        """
+        Returns a dict showing the amount of time elapsed since the start time,
+        minus any pauses.
+        """
+        if self.tstart is None:
+            raise ValueError("Timer not started.")
+        if self.paused:
+            return {
+                "time": self.tpaused - self.tstart,
+                "perf_counter": self.pcpaused - self.pcstart,
+                "process_time": self.ptpaused - self.ptstart,
+            }
+        else:
+            return {
+                "time": time.time() - self.tstart,
+                "perf_counter": time.perf_counter() - self.pcstart,
+                "process_time": time.process_time() - self.ptstart,
+            }
