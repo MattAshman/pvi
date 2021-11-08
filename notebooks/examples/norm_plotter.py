fed31e2555234f2ce3a4da2dfcffab720b519d33
Mikko Heikkilä <mikko.a.heikkila@helsinki.fi>
2021-11-08 11:16:56 +0200

    update plotting scripts
    update plotter, add norm plotter

--- notebooks/examples/norm_plotter.py
+++ notebooks/examples/norm_plotter.py
@@ -12,9 +12,7 @@ import numpy as np
 
 
 # numbers of runs to plot
-#runs_to_plot = np.linspace(1,76,76,dtype=int)#[1]
-#runs_to_plot = np.linspace(77,149,73,dtype=int)#[1]
-runs_to_plot = np.linspace(1,229,229,dtype=int)#[1]
+runs_to_plot = np.linspace(38,43,6,dtype=int)#[1]
 #runs_to_plot = np.linspace(60,76,17,dtype=int)#[1]
 #runs_to_plot = np.linspace(49,210,162,dtype=int)#[1]
 #print(runs_to_plot)
@@ -28,9 +26,7 @@ runs_to_plot = np.linspace(1,229,229,dtype=int)#[1]
 #main_folder = '/Users/mixheikk/Documents/git/DP-PVI/pytorch-code-results/dp_pvi_adult_optim/'
 #main_folder = '/Users/mixheikk/Documents/git/DP-PVI/pytorch-code-results/dp_pvi_adult_optim2/'
 #main_folder = '/Users/mixheikk/Documents/git/DP-PVI/pytorch-code-results/dp_pvi_hfa_adult/'
-#main_folder = '/Users/mixheikk/Documents/git/DP-PVI/pytorch-code-results/hfa_adult_lr_clip_runs/'
-#main_folder = '/Users/mixheikk/Documents/git/DP-PVI/pytorch-code-results/hfa_adult_dp_runs/'
-main_folder = '/Users/mixheikk/Documents/git/DP-PVI/pytorch-code-results/hfa_adult_dp_100clients_runs/'
+main_folder = '/Users/mixheikk/Documents/git/DP-PVI/pytorch-code-results/hfa_adult_lr_clip_runs/'
 #main_folder = 'dp_pvi_runs/'
 
 dataset_name = 'adult'
@@ -55,16 +51,17 @@ tmp['data_bal_kappa'] = [0.,0.]
 #tmp['data_bal_kappa'] = [.95,.95]
 
 restrictions = OD()
-restrictions['dp_sigma'] = [14.]
+restrictions['dp_sigma'] = None#[2.,3.5, 4.,8.]
 restrictions['dp_C'] = None#[1.]
+#restrictions['use_dpsgd'] = [True]
 restrictions['n_global_updates'] = None
 restrictions['n_steps'] = [10]
-restrictions['batch_size'] = [5]
+restrictions['batch_size'] = [10]
 restrictions['learning_rate'] = [1e-2]
 restrictions['damping_factor'] = None#[.5]
 
 # possible balance settings: (0,0), (.7,-3), (.75,.95)
-restrictions['data_bal_rho'] = [.0]
+restrictions['data_bal_rho'] = [0.]
 restrictions['data_bal_kappa'] = [0.]
 
 # set vars to add to title: key=variable name, value=var name in fig title
@@ -107,13 +104,12 @@ colors = plt.rcParams['axes.prop_cycle'].by_key()['color'] # note: stasndard col
 #print(colors, len(colors))
 #sys.exit()
 
-baseline_acc = 0.761 # for adult data
-
 all_res = OD()
 all_res['config'] = OD()
 all_res['client_train_res'] = OD()
 all_res['train_res'] = OD()
 all_res['validation_res'] = OD()
+all_res['client_norms'] = OD()
 
 
 if include_baselines:
@@ -153,6 +149,7 @@ for i_run in runs_to_plot:
     with open(filename, 'r') as f:
         apu = f.read()
     apu = jsonpickle.unpickler.decode(apu)
+    #print(apu)
     #for k in apu:
     #    print(k)
     #sys.exit()
@@ -164,24 +161,38 @@ for i_run in runs_to_plot:
             all_res['config'][str(i_run)]['clients'],  
             all_res['config'][str(i_run)]['n_global_updates'],  
             all_res['config'][str(i_run)]['n_steps'],  
-            all_res['config'][str(i_run)]['n_rng_seeds']
+            all_res['config'][str(i_run)]['n_rng_seeds'],
             ))
     measures = ['acc','logl']
     all_res['train_res'][str(i_run)] = {}
     all_res['validation_res'][str(i_run)] = {}
+
     for k in measures:
         all_res['train_res'][str(i_run)][k] = np.zeros((
-            all_res['config'][str(i_run)]['n_global_updates'],  
+            all_res['config'][str(i_run)]['n_global_updates'],
             all_res['config'][str(i_run)]['n_rng_seeds']
             ))
         all_res['validation_res'][str(i_run)][k] = np.zeros((
-            all_res['config'][str(i_run)]['n_global_updates'],  
+            all_res['config'][str(i_run)]['n_global_updates'],
             all_res['config'][str(i_run)]['n_rng_seeds']
             ))
 
-    # does this work with sampling=seq?
+    all_res['client_norms'][str(i_run)] = {}
+    all_res['client_norms'][str(i_run)]['pre_dp_norms'] = np.zeros((
+            all_res['config'][str(i_run)]['clients'],
+            all_res['config'][str(i_run)]['n_global_updates']*all_res['config'][str(i_run)]['n_steps'],
+            all_res['config'][str(i_run)]['n_rng_seeds'],
+            ))
+    all_res['client_norms'][str(i_run)]['post_dp_norms'] = np.zeros((
+            all_res['config'][str(i_run)]['clients'],
+            all_res['config'][str(i_run)]['n_global_updates'],
+            all_res['config'][str(i_run)]['n_rng_seeds'],
+            ))
+
 
     for i_seed in range(all_res['config'][str(i_run)]['n_rng_seeds']):
+        all_res['client_norms'][str(i_run)]['pre_dp_norms'][:,:,i_seed] = apu[f'tracked_seed{i_seed}']['client_norms']['pre_dp_norms']
+        all_res['client_norms'][str(i_run)]['post_dp_norms'][:,:,i_seed] = apu[f'tracked_seed{i_seed}']['client_norms']['post_dp_norms']
         # logl, elbo, kl
         '''
         if i_seed == 0:
@@ -192,14 +203,7 @@ for i_run in runs_to_plot:
         #'''
         #sys.exit()
         for k in client_measures:
-            try:
-                all_res['client_train_res'][str(i_run)][k][:,:,:,i_seed] = apu[f'client_train_res_seed{i_seed}'][k]
-            except KeyError as err:
-                print(f'KeyError in run {i_run} (=folder)')
-                print(f'got\n{apu}')
-                print("config: batch_size={}, jobid={}".format(all_res['config'][str(i_run)]['batch_size'], all_res['config'][str(i_run)]['job_id'] ))
-                raise err
-
+            all_res['client_train_res'][str(i_run)][k][:,:,:,i_seed] = apu[f'client_train_res_seed{i_seed}'][k]
         for k in measures:
             all_res['train_res'][str(i_run)][k][:,i_seed] = apu[f'train_res_seed{i_seed}'][k]
             all_res['validation_res'][str(i_run)][k][:,i_seed] = apu[f'validation_res_seed{i_seed}'][k]
@@ -220,91 +224,69 @@ if len(failed_runs) > 0:
         runs_to_plot.remove(i_run)
     runs_to_plot = np.array(runs_to_plot)
 
-# plot comparisons between given runs: all in same plot
 
-plot_comparisons = True
-if plot_comparisons:
+# plot tracked norms
+# check restrictions
+list_to_print = []
+for i_run in runs_to_plot:
+    print_this = True
+    for k in restrictions:
+        if restrictions[k] is not None and all_res['config'][str(i_run)][k] not in restrictions[k]:
+            print_this = False
+    if print_this:
+        list_to_print.append(i_run)
+if len(list_to_print) == 0:
+    sys.exit('No runs satisfying restrictions found!')
 
-    # check restrictions
-    list_to_print = []
-    for i_run in runs_to_plot:
-        print_this = True
-        for k in restrictions:
-            if restrictions[k] is not None and all_res['config'][str(i_run)][k] not in restrictions[k]:
-                print_this = False
-        # check baselines
-        if include_baselines and not print_this:
-            for tmp in baselines:
-                print_this = True
-                for k in tmp:
-                    if tmp[k] is not None and all_res['config'][str(i_run)][k] != tmp[k]:
-                        print_this = False
-                        break
-                if print_this:
-                    break
-        if print_this:
-            list_to_print.append(i_run)
-    if len(list_to_print) == 0:
-        sys.exit('No runs satisfying restrictions found!')
-    fig, axs = plt.subplots(2, figsize=(10,10))
-
-    for k in add_to_title:
+fig, axs = plt.subplots(2, figsize=(10,10))
+for k in add_to_title:
+    try:
+        cur_title += ", {}={}".format(add_to_title[k], all_res['config'][str(list_to_print[0])][k])
+    except:
         try:
-            cur_title += ", {}={}".format(add_to_title[k], all_res['config'][str(list_to_print[0])][k])
+            cur_title = ": {}={}".format(add_to_title[k], all_res['config'][str(list_to_print[0])][k])
         except:
-            try:
-                cur_title = ": {}={}".format(add_to_title[k], all_res['config'][str(list_to_print[0])][k])
-            except:
-                cur_title = ''
+            cur_title = ''
 
-    for i_line,i_run in enumerate(list_to_print):
-        cur_label = None
-        config = all_res['config'][str(i_run)]
+for i_line,i_run in enumerate(list_to_print):
+    cur_label = None
+    config = all_res['config'][str(i_run)]
 
-        for k in add_labels:
-            try:
-                cur_label += ", {}={}".format(add_labels[k], config[k])
-            except:
-                cur_label = "{}={}".format(add_labels[k], config[k])
-        #cur_label = f"{config['sampling_type']}:dp_C={config['dp_C']},dp_sigma={config['dp_sigma']},n_steps={config['n_steps']}, batch_size={config['batch_size']}, lr={config['learning_rate']}"
-        #cur_label = f"{config['sampling_type']}:dp_C={config['dp_C']},dp_sigma={config['dp_sigma']},n_steps={config['n_steps']}, batch_size={config['batch_size']}, lr={config['learning_rate']}"
-        
-        #else:
-        #    cur_label = f"{config['sampling_type']}:dp_C=None,dp_sigma=None,n_steps={config['n_steps']}"
+    for k in add_labels:
+        try:
+            cur_label += ", {}={}".format(add_labels[k], config[k])
+        except:
+            cur_label = "{}={}".format(add_labels[k], config[k])
 
-        x = np.linspace(1,config['n_global_updates'],config['n_global_updates'])
-        axs[0].errorbar(x, all_res['validation_res'][str(i_run)]['acc'].mean(-1), 
-                yerr= 2*all_res['validation_res'][str(i_run)]['acc'].std(-1)/np.sqrt(config['n_rng_seeds']), # 2*SEM errorbar over seeds
-                label=cur_label, 
-                color=colors[i_line%len(colors)])
-        axs[1].errorbar(x, all_res['validation_res'][str(i_run)]['logl'].mean(-1), 
-                yerr= 2*all_res['validation_res'][str(i_run)]['logl'].std(-1)/np.sqrt(config['n_rng_seeds']), # 2*SEM errorbar over seeds
-                label=cur_label,
-                color=colors[i_line%len(colors)]
-                )
-        # add baseline if available
-        if i_line == 0:
-            try:
-                axs[0].hlines(baseline_acc, x[0],x[-1], color='Gray',linestyle=':')
-            except:
-                pass
+    print('shapes, pre dp:{}, post dp: {}'.format(all_res['client_norms'][str(i_run)]['pre_dp_norms'].shape,all_res['client_norms'][str(i_run)]['post_dp_norms'].shape ))
+    # eli pre: clients*(global*local steps) ja post: clients*global
 
-        axs[0].set_ylabel('Acc')
-        axs[0].set_xlabel('Global communications')
-        axs[0].legend()
-        axs[0].grid(b=True, which='major', axis='both')
-        axs[1].set_ylabel('Logl')
-        axs[1].set_xlabel('Global communications')
-        axs[1].legend()
-        axs[1].grid(b=True, which='major', axis='both')
+    # pre dp client level
+    x1 = np.linspace(1,config['n_global_updates']*config['n_steps'],config['n_global_updates']*config['n_steps'])
+    # post dp norms
+    x2 = np.linspace(1,config['n_global_updates'],config['n_global_updates'])
 
-        plt.suptitle(f"{dataset_name} dataset mean with 2*SEM over {config['n_rng_seeds']} runs" + cur_title)
+    # plot pre & post dp norms for each client, mean over seeds
+    for i_client in range(config['clients']):
+        axs[0].plot(x1, all_res['client_norms'][str(i_run)]['pre_dp_norms'][i_client,:].mean(-1) )
+        axs[1].plot(x2, all_res['client_norms'][str(i_run)]['post_dp_norms'][i_client,:].mean(-1) )
 
+    axs[0].set_ylabel('Pre DP norm')
+    axs[0].set_xlabel('Local steps')
+    #axs[0].legend()
+    axs[0].grid(b=True, which='major', axis='both')
+    axs[1].set_ylabel('Post DP norm')
+    axs[1].set_xlabel('Global communications')
+    #axs[1].legend()
+    axs[1].grid(b=True, which='major', axis='both')
+
+    plt.suptitle(f"{dataset_name} dataset tracked norms for all clients, mean over {config['n_rng_seeds']} runs" + cur_title)
+    axs[0].set_title(cur_label)
 
     if to_disk:
         plt.savefig(fig_folder+fig_name)
         fig_folder
-    
+
     else:
         plt.show()
 
