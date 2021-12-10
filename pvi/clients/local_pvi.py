a419a65960ef982273c758bda6fc087ca61b667f
Mikko Heikkilä <mikko.a.heikkila@helsinki.fi>
2021-12-10 10:31:34 +0200

    small fixes to local pvi client
    minor changes to local pvi client

--- pvi/clients/local_pvi.py
+++ pvi/clients/local_pvi.py
@@ -1,4 +1,5 @@
 
+from abc import ABC
 from collections import defaultdict
 import copy
 import itertools
@@ -11,6 +12,7 @@ from torch.utils.data import TensorDataset, DataLoader
 from tqdm.auto import tqdm
 
 from .base import Client
+from .param_dp_client import Param_DP_Client
 from pvi.distributions.exponential_family_distributions import MeanFieldGaussianDistribution
 from pvi.distributions.exponential_family_factors import MeanFieldGaussianFactor
 from pvi.servers.synchronous_server import SynchronousServer
@@ -60,7 +62,9 @@ class Local_PVI_Client(Client):
         if config['batch_size'] is None:
             self.n_local_models = int(np.ceil(1/(config['sampling_frac_q']))  )
         else:
-            self.n_local_models = int(np.ceil((self.data['y'].shape[-1])/config['batch_size']))
+            self.n_local_models = int(np.floor((self.data['y'].shape[-1])/config['batch_size']))
+            #print(f"data len={len(self.data['y'])}, b={config['batch_size']}, n_models={self.n_local_models}, would need data len {config['batch_size']*self.n_local_models}")
+            #sys.exit()
         
         self.optimiser_states = None
         
@@ -83,6 +87,7 @@ class Local_PVI_Client(Client):
         # Cannot update during optimisation.
         self._can_update = False
 
+
         # Copy the approximate posterior, make old posterior non-trainable.
         q_old = p.non_trainable_copy()
 
@@ -93,22 +98,18 @@ class Local_PVI_Client(Client):
             q = p.trainable_copy()
 
         
-        if self.config['sampling_frac_q'] is not None:
-            # use different b for different models to use all data once: first models might have more data than last
-            # min batch_size from number of local models
-            tmp1 = int(np.floor((self.data['y'].shape[-1])/self.n_local_models))
-            if tmp1 == 0:
-                raise ValueError('Using batch_size=0! Try increasing sampling frac!')
-            tmp2 = len(self.data['y']) - tmp1*self.n_local_models
-            batch_sizes = np.zeros(self.n_local_models, dtype=int) + tmp1
-            batch_sizes[:tmp2] += 1
-            #print(tmp1,tmp2)
-            #sys.exit()
-        else:
-            batch_sizes =  np.zeros(self.n_local_models, dtype=int) + self.config['batch_size']
-            if np.amax(self.n_local_models*batch_sizes) > len(self.data['y']):
-                raise ValueError('Too large batch size!')
-        #print(f'batch sizes: {batch_sizes}, sum to {np.sum(batch_sizes)}')
+        # use different b for different models to use all data once: first models might have more data than last
+        tmp1 = int(np.floor((self.data['y'].shape[-1])/self.n_local_models))
+        if tmp1 == 0:
+            raise ValueError('Using batch_size=0! Try increasing sampling frac!')
+        tmp2 = len(self.data['y']) - tmp1*self.n_local_models
+        batch_sizes = np.zeros(self.n_local_models, dtype=int) + tmp1
+        batch_sizes[:tmp2] += 1
+        #print(tmp1,tmp2)
+        #print(np.unique(batch_sizes))
+        #print(batch_sizes)
+        #print(np.sum(batch_sizes), len(self.data['y']))
+        #sys.exit()
 
         # create internal pseudo clients if don't exist
         if self.pseudo_clients is None:
@@ -116,18 +117,23 @@ class Local_PVI_Client(Client):
             cur_ind = 0
             for i_client, b in enumerate(batch_sizes):
                 # Data of ith client
+                #if i_client == 0:
+                #    print(f"client data size: {len(self.data['y'])}, local model batch size: {b}")
                 data = {'x' : self.data['x'][cur_ind:(cur_ind+b),:], 'y' : self.data['y'][cur_ind:(cur_ind+b)] }
                 cur_ind += b
 
                 client_config =  copy.deepcopy(self.config)
                 client_config['dp_mode'] = 'param'
-                client_config['clients'] = self.n_local_models # CHECK: SHOULD INCLUDE PSEUDO-CLIENTS?
+                client_config['dp_sigma'] = self.config['dp_sigma']/(np.sqrt(self.n_local_models)) # without secure aggregation
+                #print(f"pseudo-client var: {client_config['dp_sigma']**2} will sum to {self.n_local_models*client_config['dp_sigma']**2}, should equal {self.config['dp_sigma']**2}")
+                client_config['clients'] = self.n_local_models
                 client_config['batch_size'] = int(b)
+                client_config['sampling_frac_q'] = 1. # should be obsolete
 
                 t = MeanFieldGaussianFactor(nat_params = self.t.nat_params)
 
                 # Create client and store
-                client = Client(data=data, model=self.model, t=t, config=client_config)
+                client = Param_DP_Client(data=data, model=self.model, t=t, config=client_config)
                 self.pseudo_clients.append(client)
 
 
@@ -137,8 +143,8 @@ class Local_PVI_Client(Client):
                 'max_iterations' : 1,
                 'train_model' : False,
                 'model_update_freq': 1,
-                'dp_C' : self.config['dp_C'],
-                'dp_sigma' : self.config['dp_sigma'],
+                'dp_C' : self.config['dp_C'], # no clipping on server, applied by pseudo-clients
+                'dp_sigma' : 0., # note: noise added by pseudo-clients
                 'enforce_pos_var' : self.config['enforce_pos_var'],
                 'dp_mode' : 'param',
                 "pbar" : self.config['pbar'], 
@@ -200,7 +206,7 @@ class Local_PVI_Client(Client):
             else:
                 cur_t['np1'] += client.t.nat_params['np1'].detach().clone()*self.config['damping_factor']
                 cur_t['np2'] += client.t.nat_params['np2'].detach().clone()*self.config['damping_factor']
-        '''
+        #'''
         
         if self.t is not None:
             # Compute new local contribution from old distributions
