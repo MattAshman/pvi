9604cf11b79ea56df3c7df4b2e58450f5a92fdf2
MattAshman <matthew.ashman97@gmail.com>
2021-12-17 11:03:29 +0000

    Adding Dirichlet factor

--- pvi/distributions/exponential_family_factors.py
+++ pvi/distributions/exponential_family_factors.py
@@ -9,17 +9,17 @@ MIN_PRECISION = 1e-6
 # =============================================================================
 # Mean field Gaussian factor
 # =============================================================================
-    
+
 
 class MeanFieldGaussianFactor(ExponentialFamilyFactor):
-    
+
     distribution_class = MeanFieldGaussianDistribution
-    
+
     def log_h(self, thetas):
         """
         Returns the value of log h(θ) for the MeanFieldFactor class. For
         a mean-field multivariate Gaussian, log h(θ) = 0.
-            
+
         Input **thetas** is assumed to be a torch.tensor of shape (N, D)
         where N is the batch dimension and D is the dimension of the
         distribution.
@@ -27,32 +27,29 @@ class MeanFieldGaussianFactor(ExponentialFamilyFactor):
         return torch.zeros(size=thetas.shape[:1])
 
     def npf(self, thetas):
-        
+
         np1 = self.nat_params["np1"]
         np2 = self.nat_params["np2"]
-        
+
         npf = torch.mv(thetas, np1)
         npf = npf + torch.mv(thetas ** 2, np2)
-        
+
         return npf
 
     def nat_from_dist(self, q):
-        
+
         loc = q.loc.detach()
         scale = q.scale.detach()
-        
-        std = {
-            "loc": loc,
-            "scale": scale
-        }
-        
+
+        std = {"loc": loc, "scale": scale}
+
         return self.distribution_class._nat_from_std(std)
-    
+
     def dist_from_nat(self, nat):
-        
+
         std = self.distribution_class._std_from_nat(nat)
         dist = torch.distributions.Normal(**std)
-        
+
         return dist
 
     def valid_nat_from_nat(self, nat_params):
@@ -67,7 +64,7 @@ class MeanFieldGaussianFactor(ExponentialFamilyFactor):
 
         return nat_params
 
-    
+
 # =============================================================================
 # Multivariate Gaussian factor
 # =============================================================================
@@ -81,7 +78,7 @@ class MultivariateGaussianFactor(ExponentialFamilyFactor):
         """
         Returns the value of log h(θ) for the MultivariateGaussian class. For
         a multivariate Gaussian, log h(θ) = 0.
-            
+
         Input **thetas** is assumed to be a torch.tensor of shape (N, D)
         where N is the batch dimension and D is the dimension of the
         distribution.
@@ -89,32 +86,29 @@ class MultivariateGaussianFactor(ExponentialFamilyFactor):
         return torch.zeros(size=thetas.shape[:1])
 
     def npf(self, thetas):
-        
+
         np1 = self.nat_params["np1"]
         np2 = self.nat_params["np2"]
 
         npf = torch.mv(thetas, np1)
         npf = npf + torch.sum(thetas * torch.mm(thetas, np2), dim=1)
-        
+
         return npf
 
     def nat_from_dist(self, q):
-        
+
         loc = q.loc.detach()
         cov = q.covariance_matrix.detach()
-        
-        std = {
-            "loc": loc,
-            "covariance_matrix": cov
-        }
-        
+
+        std = {"loc": loc, "covariance_matrix": cov}
+
         return self.distribution_class._nat_from_std(std)
-    
+
     def dist_from_nat(self, nat):
-        
+
         std = self.distribution_class._std_from_nat(nat)
         dist = torch.distributions.MultivariateNormal(**std)
-        
+
         return dist
 
 
@@ -152,10 +146,7 @@ class GammaFactor(ExponentialFamilyFactor):
         concentration = q.concentration.detach()
         rate = q.rate.detach()
 
-        std = {
-            "concentration": concentration,
-            "rate": rate
-        }
+        std = {"concentration": concentration, "rate": rate}
 
         return self.distribution_class._nat_from_std(std)
 
@@ -164,6 +155,7 @@ class GammaFactor(ExponentialFamilyFactor):
         dist = torch.distributions.Gamma(**std)
 
         return dist
+
 
 # =============================================================================
 # Log-Normal factor
@@ -177,5 +169,41 @@ class LogNormalFactor(MeanFieldGaussianFactor):
     def dist_from_nat(self, nat):
         std = self.distribution_class._std_from_nat(nat)
         dist = torch.distributions.LogNormal(**std)
+
+        return dist
+
+
+class DirichletFactor(ExponentialFamilyFactor):
+
+    distribution_class = DirichletDistribution
+
+    def log_h(self, thetas):
+        """
+        Returns the value of log h(θ) for the Dirichlet class. For a Dirichlet
+        distribution, log h(θ) = 0.
+
+        Input **thetas** is assumed to be a torch.tensor of shape (N, D)
+        where N is the batch dimension and D is the dimension of the
+        distribution.
+        """
+        return torch.zeros(size=thetas.shape[:1])
+
+    def npf(self, thetas):
+
+        np1 = self.nat_params["np1"]
+        npf = thetas.log * np1
+
+        return npf
+
+    def nat_from_dist(self, q):
+        conc = q.concentration.detach()
+
+        std = {"concentration": concentration}
+
+        return self.distribution_class._nat_from_std(std)
+
+    def dist_from_nat(self, nat):
+        std = self.distribution_class._std_from_nat(nat)
+        dist = torch.distributions.Dirichlet(**std)
 
         return dist
